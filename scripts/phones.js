52448267acb57347551fea8bca107335ceaaa596
Faysal Sohan <shnkhondoker@gmail.com>
2023-08-29 11:07:47 +0600

    added dynamic phone card

--- scripts/phones.js
+++ scripts/phones.js
@@ -1,1 +1,30 @@
-console.log("I am connected");
+const loadPhones = async () => {
+    const resp = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await resp.json();
+    const phones = data.data;
+    displayPhones(phones);
+}
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        console.log(phone);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+            <figure class="px-10 pt-10">
+              <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+              <h2 class="card-title">${phone.phone_name}</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+    });
+}
+
+loadPhones();
